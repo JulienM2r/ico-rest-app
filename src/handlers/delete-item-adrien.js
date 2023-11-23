1d381ab3270667fc21d8c088a1eaf60048b9c769
Adrien Meyrand <adrien.meyrand@etu.umontpellier.fr>
2023-11-23 16:13:44 +0100

    Commit

---
+++ src/handlers/delete-item-adrien.js
@@ -0,0 +1,28 @@
+exports.putItemHandler = async (event) => {
+    const { body, httpMethod, path } = event;
+    if (httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch by default. For more information, see
+    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
+    console.log('received:', JSON.stringify(event));
+
+    // Get id and name from the body of the request
+    const { id, name } = JSON.parse(body);
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    const params = {
+        TableName: tableName,
+        Item: { id, name },
+    };
+    await docClient.delete(params).promise()
+
+    const response = {
+        statusCode: 200,
+        body,
+    };
+
+    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
